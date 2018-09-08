fcc532888d5125cb113bae9136ae2a759978971f
Brian Dawn <brian.t.dawn@gmail.com>
2018-09-08 11:25:19 -0500

    Update the example.

--- src/main/kotlin/demo/helloWorld.kt
+++ src/main/kotlin/demo/helloWorld.kt
@@ -10,7 +10,26 @@ import arrow.typeclasses.*
 
 fun main(args: Array<String>) {
 
-    val result = Option.monoid(Int.monoid()).run { listOf<Option<Int>>(Some(1), Some(1)).combineAll() }
+    val someOne = Some(1)
+    val someTwo = Some(2)
+    val nope: Option<Int> = None
 
-    println(result)
+    println(someOne.map { it + 1 })
+    println(nope.map { it + 1 })
+
+    val combined = ForListK extensions {
+        listOf(someOne, someTwo, nope).k().foldMap(Option.monoid(Int.monoid()), ::identity)
+    }
+
+    println(combined)
+
+    val foo = ForOption extensions {
+        listOf(Option(1), Option(2), Option(3)).k().traverse(this, ::identity)
+    }
+    println(foo)
+
+    val bar = ForOption extensions {
+        listOf(Option(1), None, Option(3)).k().traverse(this, ::identity)
+    }
+    println(bar)
 }
